c34bd4d86a0f9210aa8f241a8424648853e26f9a
Awesohame <82087247+awesohame@users.noreply.github.com>
2024-06-09 20:54:56 +0530

    add resume

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,52 +1,48 @@
 import Image from "next/image";
+import Link from "next/link";
 import { IoLogoGithub } from "react-icons/io";
 import { MdOutlineContactPage } from "react-icons/md";
 
 export default function Home() {
     return (
         <div className='flex h-[calc(100vh-8.5rem)] w-full'>
-            <section className="h-full w-full flex xl:pb-[3rem]">
-                {/* <section className="flex flex-col justify-between w-full h-full overflow-x-hidden overflow-y-hidden"> */}
-                <div className="flex flex-col justify-between h-full w-full px-4 xl:px-24">
-                    <div className="flex justify-center mt-16 h-3/5">
-                        <div className="flex justify-center xl:justify-between items-center w-4/5 flex-wrap-reverse xl:flex-nowrap">
-                            <span
-                                className="text-6xl text-center pt-6 xl:pt-0 xl:text-left xl:text-7xl text-light-1 flex flex-col"
-                            >
-                                Hello I'm
-                                <span className="text-primary-1">Soham</span>
-                            </span>
-                            <div className="w-[240px] xl:w-[300px]">
-                                <Image
-                                    src="/assets/borderpfp.png"
-                                    alt="pfp"
-                                    width={300}
-                                    height={300}
-                                    priority
-                                    quality={100}
-                                    className="w-full h-auto"
-                                />
-                            </div>
-                        </div>
+            <section className="h-full w-full flex xl:pb-[3rem] flex-col">
+                <div className="xl:h-[80%] w-full flex flex-col-reverse items-center xl:flex-row p-[6%] xl:px-[15%]">
+                    <div className="xl:w-[60%] h-full text-4xl xl:text-5xl gap-5 flex flex-col justify-center mt-5">
+                        Hello!
+                        <span className="text-3xl xl:text-4xl">
+                            I'm <span className="text-primary-1">Soham Aversekar</span>
+                        </span>
+                        <span className="text-lg xl:text-xl text-light-2">
+                            I am a full stack developer based in Mumbai, India, currently in my second year of BE in Computer Engineering at Thadomal Shahani Engineering College. Committed to creating user-friendly websites, I am continuously expanding my technical knowledge.
+                        </span>
                     </div>
-                    <div className="flex justify-center mt-0 xl:mt-16 h-1/5">
-                        <div
-                            className="text-2xl xl:text-3xl text-light-1 flex flex-col xl:flex-row"
-                        >
-                            <p className='px-2 h-8 xl:h-12 text-light-1 text-center'>
-                                A passionate software developer with a keen interest in AI/ML.
-                            </p>
-                            {/* <div className='px-2 h-8 xl:h-12 overflow-hidden text-center xl:text-left my-2 xl:my-0'>
-                            <span className='relative rotate-text'>
-                                Student
-                                <br /> MERN Stack Developer
-                                <br /> AIML Enthusiast
-                                <br /> Student
-                            </span>
-                        </div> */}
-
-                        </div>
+                    <div className="xl:w-[40%] flex items-center justify-end">
+                        <Image
+                            src="/assets/hero.png"
+                            alt="hero"
+                            height={300}
+                            width={300}
+                            className="hidden xl:block"
+                        />
+                        <Image
+                            src="/assets/hero.png"
+                            alt="hero"
+                            height={200}
+                            width={200}
+                            className="xl:hidden"
+                        />
                     </div>
+                </div>
+                <div className="h-[20%] w-full flex items-center justify-center gap-4">
+                    <Link href="/resume.pdf" className="flex items-center bg-light-1 gap-2 text-dark-1 py-3 px-4 rounded-lg  hover:bg-primary-1">
+                        <MdOutlineContactPage size={30} />
+                        <span className="text-lg xl:text-2xl">Resume</span>
+                    </Link>
+                    <Link href="https://github.com/awesohame" className="flex items-center bg-light-1 gap-2 text-dark-2 py-3 px-4 rounded-lg hover:bg-primary-1">
+                        <IoLogoGithub size={30} />
+                        <span className="text-lg xl:text-2xl">Github</span>
+                    </Link>
                 </div>
             </section>
         </div>
